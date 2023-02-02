1cd518783949ac6a2f0bb86e8ede9d27b7b72f56
Pooja-Jathar1406 <jatharpooja1406@gmail.com>
2023-02-02 15:12:45 +0530

    second commit on branch

--- Searching.java
+++ Searching.java
@@ -1,16 +1,51 @@
+import java.util.Scanner;
+
 class Searching {
-    public static int linearSearch(int[] arr, int key) {
+    public static void linearSearch(int[] arr, int key) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key) {
-                return i;
+                System.out.println("Element is found at index: " + i);
+                ;
             }
         }
-        return -1;
+    }
+
+    public static void binarySearch(int arr[], int first, int last, int key) {
+        int mid = (first + last) / 2;
+        while (first <= last) {
+            if (arr[mid] < key) {
+                first = mid + 1;
+            } else if (arr[mid] == key) {
+                System.out.println("Element is found at index: " + mid);
+                break;
+            } else {
+                last = mid - 1;
+            }
+            mid = (first + last) / 2;
+        }
+        if (first > last) {
+            System.out.println("Element is not found!");
+        }
     }
 
     public static void main(String a[]) {
         int[] a1 = { 10, 20, 30, 50, 70, 90 };
         int key = 50;
-        System.out.println(key + " is found at index: " + linearSearch(a1, key));
+        int last = a1.length - 1;
+        System.out.println("Enter your choice : \n 1.Linear search \n 2.Binary search");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                linearSearch(a1, key);
+                break;
+            case 2:
+                binarySearch(a1, 0, last, key);
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+        sc.close();
+
     }
 }
